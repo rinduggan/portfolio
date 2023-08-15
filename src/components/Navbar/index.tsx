8f6484dca17865b7fc35052f0492c15aaa6c9f23
Rin <irinaheimerle@gmail.com>
2023-08-15 16:34:13 -0300

    remove mobile nav temp.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,41 +10,6 @@ import { Disclosure } from "@headlessui/react";
 export default function Navbar() {
   return (
     <>
-      <Disclosure as="nav" className="sm:hidden">
-        {({ open }) => (
-          <>
-            {/* mobile menu */}
-            <div className="flex justify-between items-center m-2.5">
-              <span className={`${emblemaOne.className} text-2xl`}>RIN</span>
-              <Disclosure.Button className="inline-flex items-center justify-center rounded-full p-2 bg-gold"> 
-                {open ? (
-                  <Image 
-                    src={XIcon}
-                    alt="Close Navigation Menu"
-                  />
-                ) : (
-                  <Image 
-                    src={MenuIcon}
-                    alt="Open Navigation Menu"
-                  />
-                )}
-              </Disclosure.Button>
-            </div>
-            {/* end of mobile menu */}
-
-            {open && (
-              <Disclosure.Panel>
-                <div className="flex flex-col items-center h-screen w-screen">
-                  <Link href="/about">ABOUT</Link>
-                  <Link href="/blog">BLOG</Link>
-                  <Link href="https://www.dropbox.com/scl/fi/6alcxfnd4a9v8prp5rgch/Irina-Heimerle.pdf?rlkey=vdrfpdmbhjso2fsi56vs04qvw&dl=0">RESUME</Link>
-                </div>
-              </Disclosure.Panel>
-            )}
-          </>
-        )}
-      </Disclosure>
-
       <div className="flex items-center justify-evenly rounded-full border border-gold m-3 p-2">
         <Link href="/" className={`${emblemaOne.className} text-xl`}>
           RIN
